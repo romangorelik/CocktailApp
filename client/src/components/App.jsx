5626b136c6634d607a3dba6e4333e1e332a0c2b4
Roman Gorelik <romang31@gmail.com>
2018-09-12 00:55:06 -0400

    Added changing on click and set to timed slideshow background, will fill with different information and will have a nav bar to search things later.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+/* Styling imports */
+import BackgroundSlideshow from 'react-background-slideshow'
+
+/* Component imports */
+
+let image1 = 'https://images.unsplash.com/photo-1438522014717-d7ce32b9bab9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=55cb1a59dd5a3cecf474342bc3048ea7&auto=format&fit=crop&w=2849&q=80'
+let image2 = 'https://images.unsplash.com/photo-1518619660967-1d65afe62056?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=23cf3556716d5db7c4a6210bb98d8150&auto=format&fit=crop&w=2850&q=80'
+let image3 = 'https://images.unsplash.com/photo-1482349212652-744925892164?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9b719ce0351a8dd27a1ecc9f18d9210f&auto=format&fit=crop&w=2600&q=80'
+
 class App extends React.Component {
   render () {
 		return (
-			<div>{'Hello World!'}</div>
+			<div className='splashPage'>
+				<BackgroundSlideshow images={[ image1, image2, image3 ]} />
+			</div>
 		)
 	}
 }
 
 export default App
+
---
+++ database/index.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://cocktail:cocktail1@ds153552.mlab.com:53552/cocktail');
+
